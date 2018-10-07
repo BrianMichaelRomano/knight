c1aa35850ac62bbda231ea17e5f4924e7162da22
Brian <brianmromano@gmail.com>
2018-10-06 20:48:55 -0400

    moved functions to utils

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+function divideBy(value, divisor) {
+  return Math.round(value / divisor);
+}
+
+function multiplyBy(value, multiplier) {
+  return value * multiplier;
+}
+
+export { divideBy, multiplyBy };
